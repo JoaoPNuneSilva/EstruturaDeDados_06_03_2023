b39baf4e4c58fdda725366f21e98cd041b9cc74a
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-03-06 19:48:58 -0300

    Aula_06_03_2023

---
+++ Aula_06_03_2023.py
@@ -0,0 +1,31 @@
+# nome = "João Pedro"
+# sobrenome = "Nunes da Silva"
+# idade = 21
+# altura = 1.70
+# peso = "70kg"
+# maiordeidade = idade >= 18
+# print (nome, sobrenome, sep=" ")
+# print("Idade: " , idade)
+# print("Altura: " , altura)
+# print("Peso: ", peso)
+# print ("É maior de idade: " , maiordeidade)
+
+adicao = 2 + 2
+substracao = 2-2
+multiplicacao = 2*2
+divisao = 10/3
+divisao_inteiro=10//3 #pega o número inteiro, o resto da divisão ignora
+modulo = 10 % 4
+potencia = 2**3
+
+print(adicao)
+print(substracao)
+print("A multiplicação é " , multiplicacao)
+print("A divisao é ", divisao)
+print("A divisao é ", divisao_inteiro)
+print("O modulo é "), modulo
+print("A potência é ", potencia)
+
+unir = "João " + "Pedro"
+print (unir)
+cinco_vezes ="O Matheus, pode ser um pouco duro as vezes" * 5
