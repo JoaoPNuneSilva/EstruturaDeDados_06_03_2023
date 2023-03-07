a5de0dbb064370a1ec4a71381114eb90ac809e6a
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-03-06 21:31:39 -0300

    aula_06_03_2023

--- ATV1_IMC.py
+++ ATV1_IMC.py
@@ -1,2 +1,22 @@
 #peso / altura ao quadrado
-peso = input ("Qual o seu peso?")
+peso = float (input ("Qual o seu peso?"))
+altura = float (input ("Qual a sua altura?"))
+imc = peso / altura**2
+print ("Seu IMC é: ", imc)
+# print("Seu IMC é: %.4f" % imc)
+# if imc < 16:
+# 	print("Magreza grave")
+# elif imc < 17:
+# 	print("Magreza moderada")
+# elif imc < 18.5:
+# 	print("Magreza leve")
+# elif imc < 25:
+# 	print("Saudável")
+# elif imc < 30:
+# 	print("Sobrepeso")
+# elif imc < 35:
+# 	print("Obesidade Grau I")
+# elif imc < 40:
+# 	print("Obesidade Grau II (severa)")
+# else:
+# 	print("Obesidade Grau III (mórbida)")
