da8afa4eb3dbe9612455445058e16f2554162faa
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-03-06 22:00:24 -0300

    aula

---
+++ ATV2_Exibir_Nº_maior.py
@@ -0,0 +1,8 @@
+v1 = int(input ("Insira o 1º valor: "))
+v2 = int(input ("Insira o 2º valor: "))
+if v1 >= v2:
+    print("O valor maior que foi digitado é: ", v1)
+else:
+    print("O valor maior que foi digitado é: ", v2)
+if v1 == v2:
+    print("Como ambos os valores são iguais, então o maior valor é: ", v1)
